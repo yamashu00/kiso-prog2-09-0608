e030fe25935a68e1c18045a2939d211164007803
Yuta Orihara <yutaorihara@YutanoMacBook-Air.local>
2026-06-08 14:34:28 +0900

    演習完了

--- ex_write.c
+++ ex_write.c
@@ -15,9 +15,14 @@ int main(void) {
     /* TODO: fp が NULL かどうかチェックして、NULL なら
      *       エラーメッセージを表示して return 1; する */
 
+    if(fp == NULL){
+        printf("エラーです");
+        return 1;
+    }
+
     /* TODO: fprintf で 名前,整数,小数 の形式で1行書く
      *       自分の名前・好きな数に変えること */
-
+    fprintf(fp, "折原悠太, 72,1.72\n");
     fclose(fp);
     fp = NULL;
     printf("書き込み完了\n");
