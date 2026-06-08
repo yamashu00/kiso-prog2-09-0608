325b647910da2377136c25a69ea9ff22775dbc63
高垣陽一 <yo-1@ZGMF-X42S.local>
2026-06-08 14:20:58 +0900

    演習完了

--- ex_write.c
+++ ex_write.c
@@ -14,9 +14,13 @@ int main(void) {
 
     /* TODO: fp が NULL かどうかチェックして、NULL なら
      *       エラーメッセージを表示して return 1; する */
-
+    if (fp == NULL) {
+        printf("ファイルを開けませんでした\n");
+        return 1;
+    }
     /* TODO: fprintf で 名前,整数,小数 の形式で1行書く
      *       自分の名前・好きな数に変えること */
+    fprintf(fp, "髙垣陽一,3,1.61\n");
 
     fclose(fp);
     fp = NULL;
